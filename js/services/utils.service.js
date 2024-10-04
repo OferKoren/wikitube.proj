adc715eb8a800db8ac99e97add7da75cc3b18116
OferKoren <oferkoren2@gmail.com>
2024-10-04 11:25:00 +0300

    INIT_repo

---
+++ js/services/utils.service.js
@@ -0,0 +1,25 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'
+    var color = '#'
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min)
+    const maxFloored = Math.floor(max)
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled) // The maximum is inclusive and the minimum is inclusive
+}
+
+function makeid(length = 5) {
+    var id = ''
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (var i = 0; i < length; i++) {
+        const idx = getRandomIntInclusive(0, possible.length - 1)
+        id += possible.charAt(idx)
+    }
+
+    return id
+}
