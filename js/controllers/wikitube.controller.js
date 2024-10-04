45ec172c34786b8ecea1f7f8735ac09e07cf9a6a
OferKoren <oferkoren2@gmail.com>
2024-10-04 18:32:39 +0300

    Connect youtube api

--- js/controllers/wikitube.controller.js
+++ js/controllers/wikitube.controller.js
@@ -1,1 +1,17 @@
 'use strict'
+const SRC = 'https://www.youtube.com/embed/'
+function onSearch(ev) {
+    ev.preventDefault()
+    const elSearch = document.querySelector('.search-input')
+    getYoutube(elSearch.value)
+        .then(renderSearch)
+        .catch((err) => {
+            console.log('eror')
+        })
+    console.log(elSearch.value)
+}
+function renderSearch(res) {
+    const elFrame = document.querySelector('.iframe')
+    elFrame.src = SRC + res[0].videoId
+    console.log(res[0].videoId)
+}
--- js/services/youtube.service.js
+++ js/services/youtube.service.js
@@ -1,1 +1,26 @@
 'use strict'
+const YOUTUBE_KEY = 'AIzaSyBzGfxkRtc4ciq_je7E_2UpA6PDH8Q9Dw8'
+const URL = `https://www.googleapis.com/youtube/v3/search?part=snippet
+&videoEmbeddable=true&type=video&key=${YOUTUBE_KEY}&q=`
+
+function getYoutube(search) {
+    const url1 = URL + search
+    return axios
+        .get(url1)
+        .then((res) => {
+            const list = res.data.items
+            console.log(list)
+            return list.map((videoData) => {
+                const url2 = `https://www.googleapis.com/youtube/v3/videos?part=snippet,status,contentDetails&id=${videoData.id.videoId}&key= ${YOUTUBE_KEY} `
+                axios.get(url2).then((res) => {
+                    console.log(res.data)
+                })
+
+                return {
+                    title: videoData.snippet.title,
+                    videoId: videoData.id.videoId,
+                }
+            })
+        })
+        .catch((err) => console.log('err:', err))
+}
